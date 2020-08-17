d8fd5c91c335360e8ccaaed3a4bcdf0d644353e8
Paul Tegnazian <paultegnazian@pauls-mbp.myfiosgateway.com>
2020-08-16 22:33:41 -0400

    Updated to add link to About

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ export default function Home()
   return (
     <div style={{ color: `blue`, fontsize: '32px' }}>
       <Link to="/contact">Contact</Link>
+      <p><Link to="/about">About</Link></p>
       <Header headerText="Hello, My Name is Paul" />
       <p>Whats up peoples?</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
