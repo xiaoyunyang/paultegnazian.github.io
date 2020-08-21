1309f2d5d7cb7ced9317aa034e915f7c697d1cfd
Paul Tegnazian <paultegnazian@Pauls-MacBook-Pro.local>
2020-08-21 18:20:57 -0400

    Gatsby Tutorial 2: Containers

---
+++ src/components/container.js
@@ -0,0 +1,8 @@
+import React from "react"
+import containerStyles from "./container.module.css"
+
+function Container({ children }) {
+  return <div className={containerStyles.container}>{children}</div>
+}
+
+export default Container;
